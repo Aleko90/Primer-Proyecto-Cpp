f6a07ed8bf018968e3c8ec0b0065834207af00ea
Alejandro <benitez6000@gmail.com>
2022-01-19 10:37:15 -0500

    Holamundo

---
+++ prueba1/prueba1.cpp
@@ -0,0 +1,15 @@
+// prueba1.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hola mundo :D" << endl;
+	system("PAUSE");
+    return 0;
+}
+
